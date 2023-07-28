17addc13b40bd94a798908ffeb2561b67cc3ca96
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-07-28 16:53:45 -0700

    Index

--- server/index.js
+++ server/index.js
@@ -1,25 +1,20 @@
 require('dotenv').config()
 const express = require('express')
-const methodOverride = require('method-override')
 const app = express()
 
-// Express Set-up
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 
-// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
-})
+   res.render('home')
+ })
+ 
 
-app.get('*', (req, res) => {
-    res.render('error404')
-})
-
+ app.get('*', (req, res) => {
+   res.render('error404')
+ })
+ 
 
 app.listen(process.env.PORT)
