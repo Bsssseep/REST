faadcafe8440f11f02c4a6c764c571a1ab574077
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-07-28 15:23:09 -0700

    Adding default view for layout

--- server/index.js
+++ server/index.js
@@ -1,64 +1,25 @@
- const express = require ('express');
- require('dotenv').config();
+require('dotenv').config()
+const express = require('express')
+const methodOverride = require('method-override')
+const app = express()
 
- const app = express ();
+// Express Set-up
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
- app.use('/places', require(`./controllers/places.js`));
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
 
- app.get ('/', (req,res) => {
-    res.send(`
-    <body style - "margin: 0;">
-    <div style = "border: 1px solid black;
-                  height: 10vh;
-                  background-color: white;"> 
-      <h2 style= "text-align:center;"> NAV BAR </h2>
-    </div>
-    <h1> Hello World </h1>
-    </body>
-    `);
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
- });
+app.get('*', (req, res) => {
+    res.render('error404')
+})
 
 
- 
-app.get('/teal', (req,res) => {
-
-   res.send(`
-    <body style - "margin: 0;">
-    <div style = "border: 1px solid black;
-                  height: 10vh;
-                  background-color: teal;"> 
-      <h2 style= "text-align:center;"> NAV BAR </h2>
-    </div>
-    <h1 style = "color: teal;"> Teal Page </h1>
-    </body>
-    `);
-   
-});
-
-app.get('/:color', (req,res) => {
-   const myColor = req.params.color;
-
-   res.send(`
-    <body style - "margin: 0;">
-    <div style = "border: 1px solid black;
-                  height: 10vh;
-                  background-color: ${myColor} ;"> 
-      <h2 style= "text-align:center;"> NAV BAR </h2>
-    </div>
-    <h1 style = "color: ${myColor};"> Page </h1>
-    </body>
-    `);
-   
-});
-
-app.get('*', (req,res) => {
-   
-    res.status(404).send('<h1> 404 Page </h1>');
-   
-});
-
- app.listen(process.env.PORT, () => 
- {
-    console.log(`app is running on port ${process.env.PORT}`);
- });
+app.listen(process.env.PORT)
