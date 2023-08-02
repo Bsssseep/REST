82147cdecea41dd9c556c3acc20cb3d079eaae12
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-08-02 11:31:30 -0700

    New Places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,72 @@
 const router = require('express').Router();
+const db = require('../models');
 
+// Route to display all places
+router.get('/', (req, res) => {
+    db.Place.find()
+        .then((places) => {
+            res.render('places/index', { places });
+        })
+        .catch(err => {
+            console.log('err', err);
+            res.render('error404');
+        });
+});
+
+// Route to create a new place
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
-})
-
-router.get('/new', (req, res) => {
-  let places = [
-    {
-      name: 'H-Thai-ML',
-      city: 'Seattle',
-      state: 'WA',
-      cuisines: 'Thai, Pan-Asian',
-      pic: 'http://placekitten.com/250/250'
-    }, 
-    {
-      name: 'Coding Cat Cafe',
-      city: 'Phoenix',
-      state: 'AZ',
-      cuisines: 'Coffee, Bakery',
-      pic: 'http://placekitten.com/250/250'
+  console.log(req,body);
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
     }
-  ];
-      
-  res.render('places/new', { places });
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
+
+    db.Place.create(req.body)
+        .then(() => {
+            res.redirect('/places');
+        })
+        .catch(err => {
+            if (err && err.name == 'ValidationError') {
+                let message = 'Validation Error: ';
+                for (var field in err.errors) {
+                    message += `${field} was ${err.errors[field].value}. ${err.errors[field].message}\n`;
+                }
+                res.render('places/new', { message });
+            } else {
+                res.render('error404');
+            }
+        });
+});
+
+// Route to show the form for creating a new place
+router.get('/new', (req, res) => {
+    res.render('places/new');
+});
+
+// Route to display details of a specific place
+router.get('/:id', (req, res) => {
+    db.Place.findOne({ _id: req.params.id })
+        .populate('comments')
+        .then(place => {
+            console.log(place.comments);
+            res.render('places/show', { place });
+        })
+        .catch(err => {
+            console.log('err', err);
+            res.render('error404');
+        });
 });
 
 module.exports = router;
