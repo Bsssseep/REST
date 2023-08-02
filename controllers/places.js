bb560a9e6cb9fffa1a40171fead8950d85b8990c
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-08-01 21:44:30 -0700

    Updated

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,19 @@
 const router = require('express').Router();
 
-
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
 
 router.get('/new', (req, res) => {
-
-    let places = [
-      {
+  let places = [
+    {
       name: 'H-Thai-ML',
       city: 'Seattle',
       state: 'WA',
       cuisines: 'Thai, Pan-Asian',
       pic: 'http://placekitten.com/250/250'
     }, 
-    
     {
       name: 'Coding Cat Cafe',
       city: 'Phoenix',
@@ -22,8 +23,7 @@ router.get('/new', (req, res) => {
     }
   ];
       
-      res.render('places/new', { places });
+  res.render('places/new', { places });
+});
 
-  });
-  
 module.exports = router;
